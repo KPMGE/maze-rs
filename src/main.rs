ad2a28ed90fda87738c73a0d8924379838a0519a
KPMGE <jesuskevin254@gmail.com>
2023-08-19 08:27:51 -0300

    :recycle: adjust screen size

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,12 @@ use ggez::{
     Context, GameError, GameResult,
 };
 
-// Here we define the size of our game board in terms of how many grid
-// cells it will take up. We choose to make a 30 x 20 game board.
-const GRID_SIZE: (i16, i16) = (30, 20);
-// Now we define the pixel size of each tile, which we make 32x32 pixels.
-const GRID_CELL_SIZE: (i16, i16) = (32, 32);
-
-// Next we define how large we want our actual window to be by multiplying
-// the components of our grid size by its corresponding pixel size.
+const MAZE_CELL_SIZE: (i16, i16) = (10, 10);
+const MAZE_SIZE: (usize, usize) = (95, 65);
 const SCREEN_SIZE: (f32, f32) = (
-    GRID_SIZE.0 as f32 * GRID_CELL_SIZE.0 as f32,
-    GRID_SIZE.1 as f32 * GRID_CELL_SIZE.1 as f32,
+    MAZE_SIZE.0 as f32 * MAZE_CELL_SIZE.0 as f32,
+    MAZE_SIZE.1 as f32 * MAZE_CELL_SIZE.1 as f32,
 );
-
-const MAZE_DIMENTION: (usize, usize) = (40, 25);
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
 struct MazeCell {
@@ -43,10 +35,10 @@ impl From<(i16, i16)> for MazeCell {
 impl From<MazeCell> for graphics::Rect {
     fn from(pos: MazeCell) -> Self {
         graphics::Rect::new_i32(
-            pos.x as i32 * GRID_CELL_SIZE.0 as i32,
-            pos.y as i32 * GRID_CELL_SIZE.1 as i32,
-            GRID_CELL_SIZE.0 as i32,
-            GRID_CELL_SIZE.1 as i32,
+            pos.x as i32 * MAZE_CELL_SIZE.0 as i32,
+            pos.y as i32 * MAZE_CELL_SIZE.1 as i32,
+            MAZE_CELL_SIZE.0 as i32,
+            MAZE_CELL_SIZE.1 as i32,
         )
     }
 }
@@ -56,6 +48,7 @@ struct Maze {
     height: usize,
     cells_stack: Vec<MazeCell>,
     visited_cells: i32,
+    path_width: i16,
 }
 
 impl Maze {
@@ -63,23 +56,40 @@ impl Maze {
         Self {
             width,
             height,
-            cells_stack: vec![
-                MazeCell::new(0, 0),
-                MazeCell::new(1, 1)
-            ],
+            cells_stack: vec![MazeCell::new(0, 0), MazeCell::new(1, 1)],
             visited_cells: 0,
+            path_width: 3,
         }
     }
 
     fn draw(&self, canvas: &mut ggez::graphics::Canvas) {
-        for cell in &self.cells_stack {
-            canvas.draw(
-                &graphics::Quad,
-                graphics::DrawParam::new()
-                    .dest_rect((*cell).into())
-                    .color(Color::BLUE),
-            );
+        for x in 0..self.height {
+            for y in 0..self.width {
+                let mut cell = MazeCell::new(x as i16, y as i16); 
+                cell.x *= self.path_width + 1;
+                cell.y *= self.path_width + 1;
+
+                canvas.draw(
+                    &graphics::Quad,
+                    graphics::DrawParam::new()
+                        .dest_rect(cell.into())
+                        .color(Color::BLUE),
+                );
+            }
         }
+
+        // for cell in &self.cells_stack {
+        //     let mut new_cell = cell.clone();
+        //     new_cell.x *= self.path_width + 1;
+        //     new_cell.y *= self.path_width + 1;
+
+        //     canvas.draw(
+        //         &graphics::Quad,
+        //         graphics::DrawParam::new()
+        //             .dest_rect(new_cell.into())
+        //             .color(Color::BLUE),
+        //     );
+        // }
     }
 }
 
@@ -112,7 +122,7 @@ fn main() -> GameResult {
         .build()?;
 
     let state = State {
-        maze: Maze::new(MAZE_DIMENTION.0, MAZE_DIMENTION.1),
+        maze: Maze::new(MAZE_SIZE.0, MAZE_SIZE.1),
     };
 
     event::run(ctx, event_loop, state)
