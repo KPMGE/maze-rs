7b750184015e6484b3a5b219ca8590c71bc4ac7e
KPMGE <jesuskevin254@gmail.com>
2023-08-18 23:22:37 -0300

    :sparkles: create a white canvas

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,44 @@
-use ggez::{GameError, Context, GameResult, conf, ContextBuilder, event};
+use ggez::{GameResult, event, GameError, Context, graphics::{self, Color}};
 
-struct State { }
+
+// Here we define the size of our game board in terms of how many grid
+// cells it will take up. We choose to make a 30 x 20 game board.
+const GRID_SIZE: (i16, i16) = (30, 20);
+// Now we define the pixel size of each tile, which we make 32x32 pixels.
+const GRID_CELL_SIZE: (i16, i16) = (32, 32);
+
+// Next we define how large we want our actual window to be by multiplying
+// the components of our grid size by its corresponding pixel size.
+const SCREEN_SIZE: (f32, f32) = (
+    GRID_SIZE.0 as f32 * GRID_CELL_SIZE.0 as f32,
+    GRID_SIZE.1 as f32 * GRID_CELL_SIZE.1 as f32,
+);
+
+struct State {}
 
 impl ggez::event::EventHandler<GameError> for State {
-    fn update(&mut self, ctx: &mut Context) -> GameResult {
+    fn update(&mut self, _ctx: &mut Context) -> Result<(), GameError> {
         Ok(())
     }
 
-    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+    fn draw(&mut self, ctx: &mut Context) -> Result<(), GameError> {
+        let mut canvas = graphics::Canvas::from_frame(ctx, Color::WHITE);
+
+        canvas.finish(ctx)?;
+
         Ok(())
     }
-}
+} 
 
-fn main() {
-    let state = State {};
+fn main() -> GameResult {
+    let window_dimensions = ggez::conf::WindowMode::default()
+        .dimensions(SCREEN_SIZE.0, SCREEN_SIZE.1);
 
-    let config = conf::Conf::new();
-    let (ctx, event_loop) = ContextBuilder::new("maze-rs", "kevin")
-        .default_conf(config)
-        .build()
-        .unwrap();
+    let (ctx, event_loop) = ggez::ContextBuilder::new("maze", "Kevin Carvalho")
+        .window_mode(window_dimensions)
+        .build()?;
 
-    event::run(ctx, event_loop, state);
+    let state = State {}; 
+
+    event::run(ctx, event_loop, state)
 }
